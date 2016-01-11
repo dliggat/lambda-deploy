49acc5b080ca793d64ce4c92db014a2f7505f177
Dave Liggat <dave@liggat.org>
2016-01-11 14:22:02 -0800

    Creating tasks directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,5 @@
 var grunt = require('grunt');
-var AWS   = require('aws-sdk');
-var util  = require('util');
 var fs    = require('fs');
-
-AWS.config.apiVersions = {
-  cloudformation: '2010-05-15',
-  ec2: '2015-10-01',
-  s3: '2006-03-01',
-  // other service API versions
-};
 
 grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -46,41 +37,11 @@ grunt.initConfig({
     }
 });
 
-
 grunt.loadNpmTasks('grunt-contrib-jshint');
 grunt.loadNpmTasks('grunt-contrib-watch');
 grunt.loadNpmTasks('grunt-aws-lambda');
+grunt.loadTasks('tasks');
 grunt.registerTask('default', ['jshint']);
 grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy']);
 
 
-// A very basic default task.
-grunt.registerTask('describe_stack', 'Describe the CloudFormation stack', function() {
-  var done = this.async();
-
-  grunt.log.write('Logging some stuff from Dave...').ok();
-  var cloudformation = new AWS.CloudFormation();
-  var params = {
-    StackName: grunt.config.get('describe_stack.' + this.target + '.stack')
-  };
-  cloudformation.describeStacks(params, function(err, data) {
-    if (err) {
-      grunt.fail.fatal(err, err.stack);
-    }
-    var stack  = {};
-    var status = data.Stacks[0].Outputs;
-    for (var i in status) {
-      stack[status[i].OutputKey] = status[i].OutputValue;
-    }
-    grunt.log.write(util.inspect(stack, {showHidden: false, depth: null})).ok();  // Inspect objects.
-    fs.writeFile('stack-outputs.json', JSON.stringify({'StackOutputs': stack}, null, 2), function(err) {
-      if (err) {
-        grunt.log.error(err);
-      }
-      done();
-    });
-
-  });
-
-
-});
---
+++ tasks/describe_stack.js
@@ -0,0 +1,33 @@
+var AWS   = require('aws-sdk');
+var util  = require('util');
+var fs    = require('fs');
+
+module.exports = function(grunt) {
+  grunt.registerTask('describe_stack', 'Describe the CloudFormation stack', function() {
+    var done = this.async();
+
+    var cloudformation = new AWS.CloudFormation({cloudformation: '2010-05-15'});
+    var params = {
+      StackName: grunt.config.get('describe_stack.' + this.target + '.stack')
+    };
+    cloudformation.describeStacks(params, function(err, data) {
+      if (err) {
+        grunt.fail.fatal(err, err.stack);
+      }
+      var stack  = {};
+      var status = data.Stacks[0].Outputs;
+      for (var i in status) {
+        stack[status[i].OutputKey] = status[i].OutputValue;
+      }
+      grunt.log.write(util.inspect(stack, {showHidden: false, depth: null})).ok();  // Inspect objects.
+      fs.writeFile('stack-outputs.json', JSON.stringify({'StackOutputs': stack}, null, 2), function(err) {
+        if (err) {
+          grunt.log.error(err);
+        }
+        done();
+      });
+
+    });
+  });
+
+}
