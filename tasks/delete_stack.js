811fe29dcd4646d205343f758ed6c73b6273cba4
Dave Liggat <dave@liggat.org>
2016-01-11 16:00:46 -0800

    Delete stack task

---
+++ tasks/delete_stack.js
@@ -0,0 +1,22 @@
+var AWS   = require('aws-sdk');
+var util  = require('util');
+
+module.exports = function(grunt) {
+  grunt.registerTask('delete_stack', 'Delete the CloudFormation stack', function() {
+    var done = this.async();
+
+    var cloudformation = new AWS.CloudFormation({cloudformation: '2010-05-15'});
+    var params = {
+      StackName: grunt.config('create_stack').stack
+    };
+    cloudformation.deleteStack(params, function(err, data) {
+      if (err) {
+        grunt.fail.fatal(err, err.stack);
+      } else {
+        grunt.log.write(util.inspect(data)).ok();
+      }
+      done();
+    });
+  });
+
+}
